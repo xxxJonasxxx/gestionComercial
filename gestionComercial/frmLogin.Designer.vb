bffce407aeddea074f7529022ef6a758d1f8be2f
xxxJonasxxx <elvis_jonasg@hotmail.com>
2023-05-02 11:48:12 -0500

    Se crea Solucion VB

---
+++ gestionComercial/frmLogin.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLogin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btnSend = New System.Windows.Forms.Button()
+        Me.lblUser = New System.Windows.Forms.Label()
+        Me.lblPassword = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(111, 34)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox1.TabIndex = 0
+        '
+        'btnSend
+        '
+        Me.btnSend.Location = New System.Drawing.Point(94, 133)
+        Me.btnSend.Name = "btnSend"
+        Me.btnSend.Size = New System.Drawing.Size(75, 23)
+        Me.btnSend.TabIndex = 1
+        Me.btnSend.Text = "Enviar"
+        Me.btnSend.UseVisualStyleBackColor = True
+        '
+        'lblUser
+        '
+        Me.lblUser.AutoSize = True
+        Me.lblUser.Location = New System.Drawing.Point(41, 37)
+        Me.lblUser.Name = "lblUser"
+        Me.lblUser.Size = New System.Drawing.Size(47, 15)
+        Me.lblUser.TabIndex = 2
+        Me.lblUser.Text = "Usuario"
+        '
+        'lblPassword
+        '
+        Me.lblPassword.AutoSize = True
+        Me.lblPassword.Location = New System.Drawing.Point(41, 71)
+        Me.lblPassword.Name = "lblPassword"
+        Me.lblPassword.Size = New System.Drawing.Size(67, 15)
+        Me.lblPassword.TabIndex = 3
+        Me.lblPassword.Text = "Contraseña"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(111, 71)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox2.TabIndex = 4
+        '
+        'frmLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(277, 180)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.lblPassword)
+        Me.Controls.Add(Me.lblUser)
+        Me.Controls.Add(Me.btnSend)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "frmLogin"
+        Me.Text = "Log-in"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btnSend As Button
+    Friend WithEvents lblUser As Label
+    Friend WithEvents lblPassword As Label
+    Friend WithEvents TextBox2 As TextBox
+End Class
