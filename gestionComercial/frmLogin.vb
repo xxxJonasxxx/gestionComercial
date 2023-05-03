29cc9dbc855059741f08655265dd7511cdf5eb9e
xxxJonasxxx <elvis_jonasg@hotmail.com>
2023-05-03 11:52:43 -0500

    Se incorporó el módulo mdlcommon.vb que tiene la función de conexión a la base de datos.

--- gestionComercial/frmLogin.vb
+++ gestionComercial/frmLogin.vb
@@ -3,9 +3,12 @@ Public Class frmLogin
     Private Sub btnSend_Click(sender As Object, e As EventArgs) Handles btnSend.Click
         Dim hasher As New classPassword()
 
-        Dim hashed As String = hasher.HashPassword(txtUser.Text)
+        'Dim hashed As String = hasher.HashPassword(txtUser.Text)
+        'txtPassword.Text = hashed
 
-        txtPassword.Text = hashed
+        If dbConnect() = True Then
+            MessageBox.Show("Se realizó conexión")
+        End If
 
     End Sub
 End Class
---
+++ gestionComercial/mdlcommon.vb
@@ -0,0 +1,23 @@
+﻿Imports SAPbobsCOM
+Module mdlcommon
+    Public oCompany As Company
+    Public oRecordset As Recordset
+    Public ErrNum As Integer
+    Public Function dbConnect()
+        'Se definen las variables de la conexión
+        oCompany = New Company
+        oCompany.Server = Environment.GetEnvironmentVariable("SAP_DBSERVER")
+        oCompany.CompanyDB = Environment.GetEnvironmentVariable("SAP_PRODCOMPANY")
+        oCompany.UserName = Environment.GetEnvironmentVariable("SAP_USER")
+        oCompany.Password = Environment.GetEnvironmentVariable("SAP_PROD_UPWD")
+        oCompany.DbServerType = BoDataServerTypes.dst_MSSQL2019
+
+        ErrNum = oCompany.Connect() 'Se inicia la conexion
+        If ErrNum <> 0 Then
+            Console.WriteLine("Error de conexión")
+            Return False
+        Else
+            Return True
+        End If
+    End Function
+End Module
